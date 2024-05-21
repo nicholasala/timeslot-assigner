1cecb5e72b19ae5ae66ad7860ad2034e3ad02e1b
Nicholas Sala <salanicholas.14@gmail.com>
2024-05-21 19:24:38 +0200

    Show error when operator name is empty

--- src/components/OperatorManager.vue
+++ src/components/OperatorManager.vue
@@ -6,6 +6,7 @@ const operatorStore = useOperatorStore();
 const addOperatorModal = ref(null);
 const operatorName = ref(null);
 const operatorColors = ['#aa50f4', '#efa823', '#2a23ef', '#32bf2f', '#ef3123'];
+let operatorNameError = ref(false);
 let colorCursor = 0;
 
 // operatorStore.add({name: 'Alice', color: '#32bf2f', notAssignableSlots: [0]});
@@ -14,13 +15,19 @@ function addOperator() {
   if(operatorName.value?.value) {
     operatorStore.add({name: operatorName.value?.value, color: nextColor()});
     operatorName.value.value = '';
+    operatorNameError.value = false;
+    hideAddOperatorModal();
   } else {
-    // TODO: mostrare errore se il nome inserito é vuoto
+    operatorNameError.value = true;
   }
 }
 
 function showAddOperatorModal() {
   addOperatorModal.value?.showModal();
+}
+
+function hideAddOperatorModal() {
+  addOperatorModal.value?.close();
 }
 
 function nextColor(): string {
@@ -52,13 +59,22 @@ function nextColor(): string {
 
   <dialog ref="addOperatorModal" class="modal modal-bottom sm:modal-middle">
     <div class="modal-box">
+      <form method="dialog">
+        <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+      </form>
       <h3 class="font-bold text-lg">Crea operatore</h3>
-      <input ref="operatorName" type="text" placeholder="Nome operatore" class="input input-bordered w-full max-w-xs" />
-      
+
+      <div class="py-4">
+        <input ref="operatorName"
+          type="text"
+          placeholder="Nome operatore"
+          class="input input-bordered w-full max-w-xs"
+          :class="{ 'border-2 border-rose-600': operatorNameError}"/>
+        <span v-if="operatorNameError" class="block text-rose-600">Questo campo é richiesto</span>
+      </div>
+
       <div class="modal-action">
-        <form method="dialog">
-          <button class="btn" @click="addOperator">Crea</button>
-        </form>
+        <button class="btn" @click="addOperator">Crea</button>
       </div>
     </div>
   </dialog>
